b26b17298c78a6a592c9e14ff84fe6b0ce8a5dc2
森祐一朗 <apotikisin4632@gmail.com>
2024-01-12 15:43:27 +0900

    renderを省略形に変える

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
 
-  validates :name, presence: true
-  validates :yomi, presence: true
-  validates :price, presence: true
-  validates :description, presence: true
+  with_options presence: true do
+    validates :name
+    validates :yomi
+    validates :price
+    validates :description
+  end
 end
