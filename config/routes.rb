cb28b64660f6ac19a260dd0e5ad571c8e48c21ff
森祐一朗 <apotikisin4632@gmail.com>
2024-01-17 16:03:26 +0900

    コメント削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'login', to: 'sessions#index'
-  # post 'login', to: 'sessions#create'
-  # delete 'logout', to: 'sessions#destroy'
-
   root 'items#index'
   resources :items, only: %i[show]
   resources :admins, path: '/admin/items', only: %i[index new create edit update destroy]
